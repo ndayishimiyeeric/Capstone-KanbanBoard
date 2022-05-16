5e12425056c15ed7fa52f57f384297ea69eeb489
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-16 23:10:51 +0200

    Add jest runtime and commonjs

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/index.js'),
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].js',
     clean: true,
+    assetModuleFilename: '[name][ext]',
   },
+  devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist'),
@@ -19,12 +23,25 @@ module.exports = {
     compress: true,
     historyApiFallback: true,
   },
-
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(png|svg|jpeg|jpg|gif)$/i,

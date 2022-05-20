d9a9f6c7372787bcaf54d355f5b1ad7048b3f38f
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2022-05-20 17:53:07 +0200

    Fetch comments add display comments

--- src/modules/getpokemon.js
+++ src/modules/getpokemon.js
@@ -1,12 +1,15 @@
 import Populate from './populatepoke.js';
 import Likes from './likes.js';
+import Comments from './comments.js';
 
-const popUp = document.getElementById('pop-up');
+const popUp = document.querySelector('.pop-up');
+const form = document.querySelector('.add-comment');
 const popTitle = document.querySelector('.pop-title');
 const popTop = document.querySelector('.pop-top');
 const desc1 = document.querySelector('.desc-1');
 const desc2 = document.querySelector('.desc-2');
 const shadow = document.querySelector('#shadow');
+const dispComment = document.querySelector('.display-comments');
 
 export default class GetPoke {
   static getPokemon = async (id) => {
@@ -17,6 +20,22 @@ export default class GetPoke {
     this.setLikes();
     this.updateLikes();
     this.createModal();
+    this.getCount();
+  }
+
+  static getCount = () => {
+    const pokes = document.querySelectorAll('.poke-card');
+    const count = this.counterPoke(pokes);
+    const counters = document.querySelector('.poke-count');
+    counters.textContent = `Pokes(${count})`;
+  }
+
+  static counterPoke = (arr) => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i += 1) {
+      count += 1;
+    }
+    return count;
   }
 
   static setLikes = () => {
@@ -45,9 +64,12 @@ export default class GetPoke {
     const commentButtons = document.querySelectorAll('.comment-button');
     commentButtons.forEach((button) => {
       button.addEventListener('click', (e) => {
+        this.createComment(e.target.id);
         popUp.classList.add('active');
         shadow.classList.add('active');
         if (e.target.parentElement.id === e.target.id) {
+          form.setAttribute('id', `${e.target.parentElement.firstElementChild.nextElementSibling.firstElementChild.innerHTML}`);
+          popUp.setAttribute('id', `${e.target.parentElement.firstElementChild.firstElementChild.alt}`);
           popTop.innerHTML = `
         <img class="top-img" src="${e.target.parentElement.firstElementChild.firstElementChild.src}">
         `;
@@ -60,6 +82,41 @@ export default class GetPoke {
       closeButtons.addEventListener('click', () => {
         popUp.classList.remove('active');
         shadow.classList.remove('active');
+      });
+    });
+  }
+
+  static commentFn = () => {
+    const inputName = document.getElementById('comment-input');
+    const inputTextarea = document.getElementById('comment-textarea');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (e.target.tagName === 'FORM') {
+        Comments.postComment(e.target.id, inputName.value, inputTextarea.value)
+          .then((data) => {
+            if (data === 'Created') {
+              const commentsDiv = document.querySelector('.comments-list');
+              commentsDiv.innerHTML = '';
+              dispComment.innerHTML = '';
+              this.createComment(e.target.id);
+              inputName.value = '';
+              inputTextarea.value = '';
+            }
+          });
+      }
+    });
+  }
+
+  static createComment = (pokeid) => {
+    Comments.fetchComment(pokeid).then((data) => {
+      const commentsDiv = document.querySelector('.comments-list');
+      commentsDiv.innerHTML = '';
+      dispComment.innerHTML = '';
+      data.forEach((comment) => {
+        dispComment.innerHTML = `Comments (${Comments.countFn(data)})`;
+        const li = document.createElement('li');
+        li.textContent = `${comment.creation_date} ${comment.username} : ${comment.comment}`;
+        commentsDiv.appendChild(li);
       });
     });
   }

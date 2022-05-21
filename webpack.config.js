b40847cb8704676d02693b43b6ad4d6430fe39e4
gids-dadi <gideons564@gmail.com>
2022-05-21 08:28:32 +0100

    Add final stylings to popup

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'City Web App',
+      title: 'Poke Web App',
       filename: 'index.html',
       template: 'src/template.html',
     }),
